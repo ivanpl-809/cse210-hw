f4f83013645f5187d6d99f2dc49a938b50b82c07
ivan-pl809 <pon20007@byui.edu>
2026-05-30 14:23:42 -0600

    scripture complete

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        List<Word> visibleWords = _words
+            .Where(word => !word.IsHidden())
+            .ToList();
+
+        if (visibleWords.Count == 0)
+        {
+            return;
+        }
+
+        int wordsToHide = Math.Min(numberToHide, visibleWords.Count);
+
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+
+            visibleWords[index].Hide();
+
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+
+        return $"{_reference.GetDisplayText()} - {string.Join(" ", displayWords)}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
